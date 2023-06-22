c7daa051bcb44e0489ec8d93b8a9b9252d7fb8b1
Aphaco <136358631+Aphaco@users.noreply.github.com>
2023-06-22 11:24:32 +0000

    Add files via upload

---
+++ asign3.py
@@ -0,0 +1,8 @@
+number=int(input("Enter your positive number: "))
+num=2
+while number%2==0 <number^2:
+    print(number," is not a  prime number")
+    break
+else:
+    print(number," is a prime number")
+num+=1
